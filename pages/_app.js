52bd4e4c2b8c6b722a3404968c2858917988cd80
vitalii.varfolomieiev <vitalii.varfolomieiev@onix-systems.com>
2023-03-08 16:55:25 +0200

    added context

--- pages/_app.js
+++ pages/_app.js
@@ -1,20 +1,21 @@
 import '../styles/globals.css'
 import Layout from '../src/components/layout/layout';
 import Head from 'next/head';
+import { NotificationContextProvider } from '../store/notification-contex';
 
 function MyApp({ Component, pageProps }) {
   return (
-    <>
-    <Head>
-      <title>NextJS Events</title>
-      <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-      <meta name="description" content="Find a lot of great events that allow you to evolve..." />
-    </Head>
+    <NotificationContextProvider>
       <Layout>
+        <Head>
+          <title>NextJS Events</title>
+          <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+          <meta name="description" content="Find a lot of great events that allow you to evolve..." />
+        </Head>
         <Component {...pageProps} />
       </Layout>
-    </>
-    )
+    </NotificationContextProvider>
+  )
 }
 
 export default MyApp
--- src/components/layout/layout.js
+++ src/components/layout/layout.js
@@ -1,14 +1,26 @@
 import React from 'react';
 import Head from 'next/head';
 import MainHeader from './main-header';
+import Notification from '../ui/notification';
+import NotificationContext from '../../../store/notification-contex';
 
 const Layout = (props) => {
+  const notificationCtx = React.useContext(NotificationContext);
+  const activeNotification = notificationCtx.notification;
+
   return (
     <>
       <MainHeader />
       <main>
         {props.children}
       </main>
+      {activeNotification && (
+        <Notification 
+          title={activeNotification.title} 
+          message={activeNotification.message}
+          status={activeNotification.status}
+        />
+      )}
     </>
   );
 };
